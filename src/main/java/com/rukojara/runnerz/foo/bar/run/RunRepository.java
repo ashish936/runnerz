152a7365bf8d0b3a32e97b97f369e222b017ee38
ashishbisht <bishtashish936@gmail.com>
2024-03-23 02:01:57 +0530

    2nd phase | Using embeded database and fetching data from json file

--- src/main/java/com/rukojara/runnerz/foo/bar/run/RunRepository.java
+++ src/main/java/com/rukojara/runnerz/foo/bar/run/RunRepository.java
@@ -1,8 +1,13 @@
 package com.rukojara.runnerz.foo.bar.run;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
@@ -10,40 +15,74 @@ import java.util.stream.Collectors;
 
 @Repository
 public class RunRepository {
-    private List<Run> runs = new ArrayList<>();
 
-    List<Run> findAll() {
-        return runs;
+    public static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+
+    // Dependency injection using constructor injection
+    private final JdbcClient jdbcClient;
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    @PostConstruct // this will do some initialization for this call
-    private void init() {
-        runs.add(new Run(1, "Monday morning run", LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 3, Location.INDOOR));
-        runs.add(new Run(2, "Wednesday evening run", LocalDateTime.now(), LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 6, Location.OUTDOOR));
+    public List<Run> findAll() {
+        return jdbcClient.sql("select * from run")
+                .query(Run.class)
+                .list();
     }
 
     public Optional<Run> findById(Integer id) {
-        return runs.stream()
-                .filter(run -> run.id() == id)
-                .findFirst();
+        log.info("inside findById");
+        return jdbcClient.sql("select * from Run where id = :id")
+                .param("id", id)
+                .query(Run.class)
+                .optional();
     }
 
     public void create(Run run) {
-        runs.add(run);
+        int updated = jdbcClient.sql("INSERT INTO Run (id, title, started_on, completed_on, miles, location) VALUES(?, ?, ?, ?, ?, ?)")
+                .params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString()))
+                .update();
+        Assert.state(updated == 1, "failed to create run" + run.title());
     }
 
     public void update(Run run, Integer id) {
-        Optional<Run> existingRun = findById(id);
-        if(existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
+        Optional<Run> existingId = findById(id);
+        if(existingId.isPresent()) {
+            int updated = jdbcClient.sql("update Run set title = ?, started_on = ?, completed_on = ?, miles = ?, location = ? where id = ?")
+                    .params(List.of(run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString(), id))
+                    .update();
+
+            Assert.state(updated == 1, "failed to update run " + run.title());
         }
     }
 
+
     public void delete(Integer id) {
-        Optional<Run> existingRun = findById(id);
-        if(existingRun.isPresent()) {
-            runs.remove(runs.indexOf(existingRun.get()));
+        Optional<Run> existingId = findById(id);
+        if(existingId.isPresent()) {
+            int updated = jdbcClient.sql("delete from Run where id = :id")
+                    .param("id", id)
+                    .update();
+
+            Assert.state(updated == 1, "Failed to delete " + id);
         }
+    }
+
+    public int count() {
+        return jdbcClient.sql("select * from Run")
+                .query()
+                .listOfRows().size();
+    }
+
+    public void saveAll(List<Run> runs) {
+        runs.forEach(this::create);
+    }
+
+    public List<Run> findByLocation(String location) {
+        return jdbcClient.sql("select * from Run where location = : location")
+                .param("location", location)
+                .query(Run.class)
+                .list();
     }
 
 }
---
+++ src/main/java/com/rukojara/runnerz/foo/bar/run/Runs.java
@@ -0,0 +1,6 @@
+package com.rukojara.runnerz.foo.bar.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+}
